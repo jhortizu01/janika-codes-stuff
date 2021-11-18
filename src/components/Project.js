101ee20c791a0f3806601f325c230b0459f461b4
Janika Hortizuela <jhortizuela21@gmail.com>
2021-11-18 15:36:02 -0700

    Change some styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import '../styles/Project.scss'
+import ScrollAnimation from 'react-animate-on-scroll'
+import lineBreak from '../assets/line-break.png'
 
 const Project = ({ projectData }) => {
 
@@ -9,24 +11,32 @@ const Project = ({ projectData }) => {
    }) 
       return (
           <div className="card" key={project.id}>
-            <div className="box1">
-              <img className="project-img" src={project.picture} alt={`picture of project`} />
-            </div>
-            <div className="box2">
-              <h2>{project.projectTitle}</h2>
-              <h3>It took {project.duration} to finish this project!</h3>
-              <h3>Tech Stack</h3>
-              <p>{stack}</p>
-              <h3>Description:</h3>
-              <p>{project.description}</p>
-              <h3>Favorite Part:</h3>
-              <p>{project.favoritePart}</p>
-              <h3>Not as fun part:</h3>
-              <p>{project.worstPart}</p>
+            <div className="card-content">
+              <div className="animation-content"> 
+                <ScrollAnimation animateIn="bounceInLeft" animateOut="bounceOutLeft">
+                  <div className="box1">
+                    <img className="project-img" src={project.picture} alt={`picture of project`} />
+                  </div>
+                </ScrollAnimation>
+                <ScrollAnimation animateIn="bounceInRight" animateOut="bounceOutRight">
+                  <div className="box2">
+                    <h2>{project.projectTitle}</h2>
+                    <h3>It took {project.duration} to finish this project!</h3>
+                    <h3>Tech Stack</h3>
+                    <p>{stack}</p>
+                    <h3>Description:</h3>
+                    <p>{project.description}</p>
+                    <h3>Favorite Part:</h3>
+                    <p>{project.favoritePart}</p>
+                    <h3>Not as fun part:</h3>
+                    <p>{project.worstPart}</p>
+                  </div>
+                </ScrollAnimation>
+              </div>
               <a href={project.github}><div className="gitHubCode" /></a>
             </div>
+            <img src={lineBreak} alt="line break image" className="line-break"/>
           </div>
-        
       )
     })
   
